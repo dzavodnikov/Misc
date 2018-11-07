9925591914b37a31bdf17746f02bb4b782e2bbf9
dzavodnikov <dzavodnikov@AT-D-DZ01.appliedtech.ru>
2018-11-07 15:12:43 +0500

    Implement useful mappings.

---
+++ src/main/java/org/zavodnikov/datastruct/OneToOneHashMap.java
@@ -0,0 +1,129 @@
+/*
+ * The MIT License (MIT)
+ *
+ * Copyright (c) 2012-2018 Dmitry Zavodnikov
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ */
+package org.zavodnikov.datastruct;
+
+import java.util.Collection;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Set;
+
+/**
+ * @author Dmitry Zavodnikov (d.zavodnikov@gmail.com)
+ */
+public class OneToOneHashMap<K, V> implements Map<K, V> {
+
+    private final Map<K, V> main = new HashMap<>();
+
+    private final Map<V, K> back = new HashMap<>();
+
+    @Override
+    public int size() {
+        return this.main.size();
+    }
+
+    @Override
+    public boolean isEmpty() {
+        return this.main.isEmpty();
+    }
+
+    @Override
+    public boolean containsKey(final Object key) {
+        return this.main.containsKey(key);
+    }
+
+    @Override
+    public boolean containsValue(final Object value) {
+        return this.back.containsKey(value);
+    }
+
+    @Override
+    public V get(final Object key) {
+        return this.main.get(key);
+    }
+
+    public K getValueKey(final V value) {
+        return this.back.get(value);
+    }
+
+    @Override
+    public V remove(final Object key) {
+        final V value = this.main.remove(key);
+        this.back.remove(value);
+
+        assert this.main.size() == this.back.size();
+        return value;
+    }
+
+    public K removeValue(final V value) {
+        final K key = this.back.remove(value);
+        this.main.remove(key);
+
+        assert this.main.size() == this.back.size();
+        return key;
+    }
+
+    @Override
+    public V put(final K key, final V value) {
+        remove(key);
+        removeValue(value);
+
+        this.main.put(key, value);
+        this.back.put(value, key);
+
+        assert this.main.size() == this.back.size();
+        return value;
+    }
+
+    @Override
+    public void putAll(final Map<? extends K, ? extends V> m) {
+        for (final K key : m.keySet()) {
+            put(key, m.get(key));
+        }
+    }
+
+    @Override
+    public void clear() {
+        this.main.clear();
+        this.back.clear();
+    }
+
+    @Override
+    public Set<K> keySet() {
+        return this.main.keySet();
+    }
+
+    public Set<V> valueSet() {
+        return this.back.keySet();
+    }
+
+    @Override
+    public Collection<V> values() {
+        return valueSet();
+    }
+
+    @Override
+    public Set<Entry<K, V>> entrySet() {
+        return this.main.entrySet();
+    }
+}
---
+++ src/test/java/org/zavodnikov/datastruct/OneToOneHashMapTest.java
@@ -0,0 +1,128 @@
+/*
+ * The MIT License (MIT)
+ *
+ * Copyright (c) 2012-2018 Dmitry Zavodnikov
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ */
+package org.zavodnikov.datastruct;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * Tests for {@link OneToOneHashMap}.
+ *
+ * @author Dmitry Zavodnikov (d.zavodnikov@gmail.com)
+ */
+public class OneToOneHashMapTest {
+
+    @Test
+    public void testPutDifferent() {
+        final OneToOneHashMap<String, Integer> map = new OneToOneHashMap<>();
+        assertEquals(0, map.size());
+        assertTrue(map.isEmpty());
+
+        map.put("1", 1);
+        map.put("2", 2);
+        map.put("3", 3);
+
+        assertEquals(3, map.size());
+        assertFalse(map.isEmpty());
+
+        assertTrue(map.containsKey("1"));
+        assertTrue(map.containsValue(new Integer(1)));
+        assertEquals(new Integer(1), map.get("1"));
+        assertEquals("1", map.getValueKey(new Integer(1)));
+
+        assertTrue(map.containsKey("2"));
+        assertTrue(map.containsValue(new Integer(2)));
+        assertEquals(new Integer(2), map.get("2"));
+        assertEquals("2", map.getValueKey(new Integer(2)));
+
+        assertTrue(map.containsKey("3"));
+        assertTrue(map.containsValue(new Integer(3)));
+        assertEquals(new Integer(3), map.get("3"));
+        assertEquals("3", map.getValueKey(new Integer(3)));
+    }
+
+    @Test
+    public void testPutSameKey() {
+        final OneToOneHashMap<String, Integer> map = new OneToOneHashMap<>();
+        assertEquals(0, map.size());
+        assertTrue(map.isEmpty());
+
+        map.put("1", 1);
+        map.put("1", 2);
+        map.put("1", 3);
+
+        assertEquals(1, map.size());
+        assertFalse(map.isEmpty());
+
+        assertTrue(map.containsKey("1"));
+        assertTrue(map.containsValue(new Integer(3)));
+        assertEquals(new Integer(3), map.get("1"));
+        assertEquals("1", map.getValueKey(new Integer(3)));
+    }
+
+    @Test
+    public void testPutSameValue() {
+        final OneToOneHashMap<String, Integer> map = new OneToOneHashMap<>();
+        assertEquals(0, map.size());
+        assertTrue(map.isEmpty());
+
+        map.put("1", 1);
+        map.put("2", 1);
+        map.put("3", 1);
+
+        assertEquals(1, map.size());
+        assertFalse(map.isEmpty());
+
+        assertTrue(map.containsKey("3"));
+        assertTrue(map.containsValue(new Integer(1)));
+        assertEquals(new Integer(1), map.get("3"));
+        assertEquals("3", map.getValueKey(new Integer(1)));
+    }
+
+    @Test
+    public void testRemove() {
+        final OneToOneHashMap<String, Integer> map = new OneToOneHashMap<>();
+        assertEquals(0, map.size());
+        assertTrue(map.isEmpty());
+
+        map.put("1", 1);
+        map.put("2", 2);
+        map.put("3", 3);
+        assertEquals(3, map.size());
+
+        map.remove("1");
+        assertEquals(2, map.size());
+
+        map.removeValue(2);
+        assertEquals(1, map.size());
+
+        assertTrue(map.containsKey("3"));
+        assertTrue(map.containsValue(new Integer(3)));
+        assertEquals(new Integer(3), map.get("3"));
+        assertEquals("3", map.getValueKey(new Integer(3)));
+    }
+}
